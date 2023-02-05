367d1b0b6e395de91328daa813d6e4a706a9ef52
VGNieto <soyviczz@gmail.com>
2023-02-05 20:20:39 +0100

    Rutas creadas para navegacion

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,18 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Episode from "./pages/Episode"
+import Home from "./pages/Home"
+import Podcast from "./pages/Podcast"
 
 const App =() => {
   return (
-    <div>Zara.com</div>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path ="/podcast/:podcastId*" element={<Podcast  />} />
+        <Route path ="/podcast/:podcastId/episode/:episodeId*"  element={<Episode/>} />
+      </Routes>
+    </Router>
   )
 }
  
---
+++ src/components/components/atoms/PodcastCard/PodcastCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const PodcastCard = ({image, description, author} : {image: string, description: string, author:string}) => {
+  return (
+    <div>PodcastCard</div>
+  )
+}
+
+export default PodcastCard
